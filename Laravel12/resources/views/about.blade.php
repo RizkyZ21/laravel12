e6c8c5dee6917557eb6c99034cba62927770cc08
RizkyZ21 <rizkyzuhriansyah4@gmail.com>
2026-03-05 08:39:00 +0700

    menambah halaman about

---
+++ Laravel12/resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+
+<h1>Halaman About</h1>
+
+<p>Ini adalah halaman about pada website Laravel.</p>
+<p>Website ini dibuat untuk belajar Laravel.</p>
+
+</body>
+</html>
